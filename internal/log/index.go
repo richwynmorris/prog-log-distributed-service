a95767268eeb952f4837206abbd79b681edf3ca6
richwynmorris <richwynmorris@protonmail.com>
2022-07-22 08:08:33 +0100

    feat: add functionality for reading and writing on the logs index

--- internal/log/index.go
+++ internal/log/index.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"io"
 	"os"
 
 	"github.com/tysonmote/gommap"
@@ -43,6 +44,41 @@ func newIndex(f *os.File, c Config) (*index, error) {
 	return idx, nil
 }
 
+// Read takes an offset index and returns the associated records position in the store
+func (i *index) Read(index int64) (out uint32, pos uint64, err error) {
+	if i.size == 0 {
+		return 0, 0, io.EOF
+	}
+	// Set the out to the last offset position in the index.
+	if index == -1 {
+		out = uint32((i.size / entWidth) - 1)
+	} else {
+		out = uint32(index)
+	}
+	pos = uint64(out) * entWidth
+	if i.size < pos+entWidth {
+		return 0, 0, io.EOF
+	}
+	out = enc.Uint32(i.mmap[pos : pos+offWidth])
+	pos = enc.Uint64(i.mmap[pos+offWidth : pos+entWidth])
+	return out, pos, nil
+}
+
+// Write appends the given offset and position to the index
+func (i *index) Write(off uint32, pos uint64) error {
+	if uint64(len(i.mmap)) < i.size+entWidth {
+		return io.EOF
+	}
+	enc.PutUint32(i.mmap[i.size:i.size+offWidth], off)
+	enc.PutUint64(i.mmap[i.size+offWidth:i.size+entWidth], pos)
+	i.size += entWidth
+	return nil
+}
+
+func (i *index) Name() string {
+	return i.file.Name()
+}
+
 func (i *index) Close() error {
 	// Makes sure memory-mapped file has synced data to the persisted file
 	err := i.mmap.Sync(gommap.MS_SYNC)
@@ -62,6 +98,6 @@ func (i *index) Close() error {
 		return err
 	}
 
-	// Closes the file safely. 
+	// Closes the file safely.
 	return i.file.Close()
 }
