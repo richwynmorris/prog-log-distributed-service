0fcfe4bc5de53117ed389806281810645d53f30f
richwynmorris <richwynmorris@protonmail.com>
2022-07-26 09:26:00 +0100

    feat: add custom error using grpc

--- api/v1/error.go
+++ api/v1/error.go
@@ -1,1 +1,29 @@
 package log_v1
+
+import (
+	"fmt"
+
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/status"
+)
+
+type ErrOffestOutOfRange struct {
+	Offset uint64
+}
+
+func (e ErrOffestOutOfRange) GRPCStatus() *status.Status {
+	st := status.New(404, fmt.Sprintf("offset out of range: %d", e.Offset))
+	msg := fmt.Sprintf("The requested offset is outside of th elogs range: %d", e.Offset)
+	details := &errdetails.LocalizedMessage{
+		Locale:  "en-GB",
+		Message: msg,
+	}
+	std, err := st.WithDetails(details)
+	if err != nil {
+		return st
+	}
+	return std
+}
+func (e ErrOffestOutOfRange) Error() string {
+	return e.GRPCStatus().Err().Error()
+}
