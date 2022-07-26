74e61a26c773d107389da64d47e203b67a8ecafb
richwynmorris <richwynmorris@protonmail.com>
2022-07-26 09:24:18 +0100

    feat: added server endpoints for grpc stubs

---
+++ internal/server/server.go
@@ -0,0 +1,89 @@
+package server
+
+import (
+	"context"
+
+	api "github.com/richwynmorris/proglog/api/v1"
+)
+
+type Config struct {
+	CommitLog CommitLog
+}
+
+type grpcServer struct {
+	api.UnimplementedLogServer
+	*Config
+}
+
+type CommitLog interface {
+	Append(*api.Record) (uint64, error)
+	Read(uint642 uint64) (*api.Record, error)
+}
+
+var _ api.LogServer = (*grpcServer)(nil)
+
+func newgrpcServer(config *Config) (srv *grpcServer, err error) {
+	srv = &grpcServer{
+		Config: nil,
+	}
+	return srv, nil
+}
+
+func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api.ProduceResponse, error) {
+	offset, err := s.CommitLog.Append(req.Record)
+	if err != nil {
+		return nil, err
+	}
+
+	return &api.ProduceResponse{Offset: offset}, nil
+}
+
+func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (*api.ConsumeResponse, error) {
+	record, err := s.CommitLog.Read(req.Offset)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ConsumeResponse{Record: record}, nil
+}
+
+func (s *grpcServer) ProduceStream(stream api.Log_ProduceStreamServer) error {
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+
+		res, err := s.Produce(stream.Context(), req)
+		if err != nil {
+			return err
+		}
+
+		err = stream.SendMsg(res)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func (s *grpcServer) ConsumeStream(req *api.ConsumeRequest, stream api.Log_ConsumeStreamServer) error {
+	for {
+		select {
+		case <-stream.Context().Done():
+			return nil
+		default:
+			res, err := s.Consume(stream.Context(), req)
+			switch err.(type) {
+			case nil:
+			case api.ErrOffestOutOfRange:
+				continue
+			default:
+				return err
+			}
+			err = stream.Send(res)
+			if err != nil {
+				return err
+			}
+			req.Offset++
+		}
+	}
+}
