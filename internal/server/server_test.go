fcacf1470afdfa2396e2b2bff3fc38b9ebef6a88
richwynmorris <richwynmorris@protonmail.com>
2022-08-10 10:23:00 +0100

    feat: add key and cert file path for TLS setup config

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -45,7 +45,9 @@ func setupTest(t *testing.T, fn func(*Config)) (
 	require.NoError(t, err)
 
 	clientTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
-		CAFile: config.CAFile,
+		CAFile:   config.CAFile,
+		KeyFile:  config.ClientCertFile,
+		CertFile: config.ClientKeyFile,
 	})
 	require.NoError(t, err)
 
@@ -63,6 +65,7 @@ func setupTest(t *testing.T, fn func(*Config)) (
 		KeyFile:       config.ServerKeyFile,
 		CAFile:        config.CAFile,
 		ServerAddress: l.Addr().String(),
+		Server:        true,
 	})
 	require.NoError(t, err)
 
